93b26043f0ad2ab7a370f8dbe39047dc8c0eb67a
Weniytide <weniytide@gmail.com>
2025-03-25 01:12:35 +0200

    Add index.js file

---
+++ index.js
@@ -0,0 +1,21 @@
+const bulb = document.getElementById('bulb');
+const body = document.querySelector('body');
+const born = document.getElementById('born-date');
+
+function changeBackground() {
+  if (bulb.style.color === 'black') {
+    bulb.style.color = 'yellow';
+    body.classList.add('dark-body');
+  } else {
+    bulb.style.color = 'black';
+    body.classList.remove('dark-body');
+  }
+}
+
+bulb.addEventListener('click', changeBackground);
+
+// function changeDate() {
+//   let now = new Date();
+//   born.innerHTML = now.getDate();
+// }
+// setInterval(changeDate(), 1000);
